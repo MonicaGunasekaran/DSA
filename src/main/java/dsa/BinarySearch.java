0520d0176b62e604b556c5ad23397bb6419bb6d2
Monica <gmonica2593@gmail.com>
2026-06-08 17:47:36 +0530

    binary search

--- src/main/java/dsa/BinarySearch.java
+++ src/main/java/dsa/BinarySearch.java
@@ -10,7 +10,7 @@ public static int bs(int[] arr,int target) {
 			return mid;
 		}
 		else if(arr[mid]>target) {
-			end=mid;
+			end=mid-1;
 		}
 		else {
 			start=mid+1;
@@ -19,7 +19,7 @@ public static int bs(int[] arr,int target) {
 	return -1;
 }
 public static void main(String[] args) {
-	int[] arr= {1,2,3,4,5,6,7};
+	int[] arr= {1,2,3,4,5,6,8};
 	int target=7;
 	System.out.println(bs(arr,target));
 }

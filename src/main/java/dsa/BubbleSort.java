b02681c6b11b289d286e045f795ed2d883cdb6e8
Monica <gmonica2593@gmail.com>
2026-06-09 07:53:51 +0530

    bubble sort

---
+++ src/main/java/dsa/BubbleSort.java
@@ -0,0 +1,22 @@
+package dsa;
+
+public class BubbleSort {
+public static void bubbleSorting(int[] arr) {
+	for(int i=0;i<arr.length;i++) {
+		for(int j=0;j<arr.length-i-1;j++) {
+			if(arr[j]>arr[j+1]) {
+				int temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}
+	}
+}
+public static void main(String[] args) {
+	int[] arr= {1,7,9,5,3,4};
+	bubbleSorting(arr);
+	for(int i:arr) {
+		System.out.print(i+" ");
+	}
+}
+}
